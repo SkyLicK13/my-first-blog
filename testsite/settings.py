7504ce1abaaef404d0fa92972ffa25fe14d7972b
skylick13 <skylick13@gmail.com>
2020-10-29 21:15:46 +0300

    My Django app, 3nd commit

--- testsite/settings.py
+++ testsite/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(file)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
